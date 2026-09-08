7900955a1fe7446613af4ca9e0862d8a68f0b928
jd2309 <26bst029@hicas.ac.in>
2026-09-08 21:40:15 +0530

    Createad add.c

---
+++ add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int add(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    int num1, num2, result;
+  
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+  
+    result = add(num1, num2);
+  
+    printf("Sum = %d", result);
+  
+    return 0;
+}
